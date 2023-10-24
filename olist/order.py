b9a595abdef72d744154ab3f54d22b5be7cb22b5
Hadrien Moncomble <hmoncomble@gmail.com>
2023-10-24 12:42:38 +0200

    Completed step review score

--- olist/order.py
+++ olist/order.py
@@ -13,21 +13,72 @@ class Order:
         # Assign an attribute ".data" to all new instances of Order
         self.data = Olist().get_data()
 
-    def get_wait_time(self, is_delivered=True):
+    def get_wait_time(self, is_delivered = True) -> pd.DataFrame:
         """
         Returns a DataFrame with:
         [order_id, wait_time, expected_wait_time, delay_vs_expected, order_status]
         and filters out non-delivered orders unless specified
         """
         # Hint: Within this instance method, you have access to the instance of the class Order in the variable self, as well as all its attributes
-        pass  # YOUR CODE HERE
+        orders = self.data['orders'].copy()
+        
+        if is_delivered:
+            # If true replace the order df by a filtered version of the df by order status = delivered
+            orders = orders.loc[orders.loc[:, 'order_status'] == 'delivered', :]
+        
+        else:
+            # Else don't modify the df
+            pass
+        
+        # Date time columns names
+        columns = orders.columns[3:]
+
+        # Transform to datetime time columns
+        for col in columns:
+            orders.loc[:, col] = pd.to_datetime(orders[col]).copy()
+        
+        # Create the orders_wait_time df
+        orders_wait_time = pd.DataFrame(columns = ["order_id", "wait_time", "expected_wait_time", "delay_vs_expected", "order_status"])
+        
+        # Create the columns wait times
+        orders_wait_time.loc[:, 'wait_time'] = orders.loc[:, 'order_delivered_customer_date'] - orders.loc[:, 'order_purchase_timestamp']
+        orders_wait_time.loc[:, 'expected_wait_time'] = orders.loc[:, 'order_estimated_delivery_date'] - orders.loc[:, 'order_purchase_timestamp']
+
+        # Fill the column delay vs expected
+        delay_vs_expected = orders_wait_time.loc[:, 'expected_wait_time'].dt.days - orders_wait_time.loc[:, 'wait_time'].dt.days
+        orders_wait_time.loc[:, "delay_vs_expected"] = delay_vs_expected.apply(lambda x: x if x >= 0 else 0)
+
+        # Fill the column order status
+        orders_wait_time.loc[:, "order_status"] = orders.loc[:, "order_status"]
+        
+        # Fill the column order id
+        orders_wait_time.loc[:, "order_id"] = orders.loc[:, "order_id"]
+        
+        return orders_wait_time
 
     def get_review_score(self):
         """
         Returns a DataFrame with:
         order_id, dim_is_five_star, dim_is_one_star, review_score
         """
-        pass  # YOUR CODE HERE
+        reviews = self.data['order_reviews'].copy()
+        
+        # Create the new empty df
+        review_score = pd.DataFrame(columns = ["order_id", "dim_is_five_star", "dim_is_one_star", "review_score"])
+
+        # Create the column dim is five star
+        review_score.loc[:, "dim_is_five_star"] = reviews.loc[:, "review_score"].apply(lambda x: 1 if x == 5 else 0)
+
+        # Create the column dim is one star
+        review_score.loc[:, "dim_is_one_star"] = reviews.loc[:, "review_score"].apply(lambda x: 1 if x == 1 else 0)
+
+        # Fill the column review score
+        review_score.loc[:, "review_score"] = reviews.loc[:, "review_score"]
+
+        # Fill the column order id
+        review_score.loc[:, "order_id"] = reviews.loc[:, "order_id"]
+        
+        return review_score
 
     def get_number_products(self):
         """
