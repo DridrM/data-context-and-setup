1b9ad7d96fb359bbacb78988223c3573f96d250f
Hadrien Moncomble <hmoncomble@gmail.com>
2023-10-24 15:52:27 +0200

    Completed orders

--- olist/order.py
+++ olist/order.py
@@ -9,7 +9,7 @@ class Order:
     DataFrames containing all orders as index,
     and various properties of these orders as columns
     '''
-    def __init__(self):
+    def __init__(self) -> None:
         # Assign an attribute ".data" to all new instances of Order
         self.data = Olist().get_data()
 
@@ -56,7 +56,7 @@ class Order:
         
         return orders_wait_time
 
-    def get_review_score(self):
+    def get_review_score(self) -> pd.DataFrame:
         """
         Returns a DataFrame with:
         order_id, dim_is_five_star, dim_is_one_star, review_score
@@ -80,7 +80,7 @@ class Order:
         
         return review_score
 
-    def get_number_products(self):
+    def get_number_products(self) -> pd.DataFrame:
         """
         Returns a DataFrame with:
         order_id, number_of_products
@@ -88,7 +88,9 @@ class Order:
         items = self.data['order_items'].copy()
         
         # Create the new df
-        number_products = items.loc[:, ["order_id", "order_item_id"]].groupby(by = "order_id").count().reset_index()       
+        number_products = items.loc[:, ["order_id", "order_item_id"]].groupby(by = "order_id").count().reset_index()
+        # Rename the counting column
+        number_products.rename(columns = {"order_item_id": "number_of_products"}, inplace = True)
         
         return number_products
 
@@ -101,10 +103,12 @@ class Order:
         
         # Create the new df
         number_sellers = items.loc[:, ["order_id", "seller_id"]].groupby(by = "order_id").count().reset_index()
+        # Rename the counting column
+        number_sellers.rename(columns = {"seller_id": "number_of_sellers"}, inplace = True)
         
         return number_sellers
 
-    def get_price_and_freight(self):
+    def get_price_and_freight(self) -> pd.DataFrame:
         """
         Returns a DataFrame with:
         order_id, price, freight_value
@@ -125,8 +129,8 @@ class Order:
         pass  # YOUR CODE HERE
 
     def get_training_data(self,
-                          is_delivered=True,
-                          with_distance_seller_customer=False):
+                          is_delivered = True,
+                          with_distance_seller_customer = False) -> pd.DataFrame:
         """
         Returns a clean DataFrame (without NaN), with the all following columns:
         ['order_id', 'wait_time', 'expected_wait_time', 'delay_vs_expected',
@@ -135,4 +139,9 @@ class Order:
         'distance_seller_customer']
         """
         # Hint: make sure to re-use your instance methods defined above
-        pass  # YOUR CODE HERE
+        
+        return self.get_wait_time(is_delivered)\
+            .merge(self.get_review_score(), on = 'order_id')\
+                .merge(self.get_number_products(), on = 'order_id')\
+                    .merge(self.get_number_sellers(), on = 'order_id')\
+                        .merge(self.get_price_and_freight(), on = 'order_id').dropna(axis = 0)
